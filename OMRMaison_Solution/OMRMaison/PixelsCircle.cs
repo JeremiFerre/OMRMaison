a6d99803d3ec5bf07df226100ef1bd531346d470
yermangderrff <yermangderrff36@gmail.com>
2016-11-02 16:23:55 +0100

    Update PixelsCircle.cs

--- OMRMaison_Solution/OMRMaison/PixelsCircle.cs
+++ OMRMaison_Solution/OMRMaison/PixelsCircle.cs
@@ -4,6 +4,21 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
+/// This file is part of Yermangderrff - OMRMaison.
+
+/// OMRMaison is free software: you can redistribute it and/or modify
+/// it under the terms of the GNU General Public License as published by
+/// the Free Software Foundation, either version 3 of the License, or
+/// (at your option) any later version.
+
+/// OMRMaison is distributed in the hope that it will be useful,
+/// but WITHOUT ANY WARRANTY; without even the implied warranty of
+/// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+/// GNU General Public License for more details.
+
+/// You should have received a copy of the GNU General Public License
+/// along with OMRMaison.  If not, see <http://www.gnu.org/licenses/>.
+
 namespace OMRMaison
 {
     public class PixelsCircle
